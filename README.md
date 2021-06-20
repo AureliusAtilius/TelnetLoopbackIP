48f207f720e891226e31a69376a91ff1991503ca
Josh <j.j.h.lindner@gmail.com>
2021-06-20 15:10:26 -0700

    Added VLAN loop to python35.
    Updated README.

--- README.md
+++ README.md
@@ -10,4 +10,8 @@ python33.py
 Collects user login credentials and initiates a Telnet session with a Cisco router using those credentials.
 Once logged in, creates and names VLAN 2 through 20.
 
+python35.py
+Collects user login credentials and initiates a Telnet session with switches based on IP addresses stored in
+the myswitches.txt file. Once logged in to each switch, creates and names VLAN 2 through 8.
+
 WARNING: Telnet is not recognized as a secure connection. Use at own risk.
--- python35.py
+++ python35.py
@@ -5,38 +5,29 @@ HOST = "localhost"
 user = input("Enter your remote account: ")
 password = getpass.getpass()
 
+# Read list of IP addresses and telnet to each switch.
 f = open ('myswitches')
 
 for IP in f:
-    IP=IP.strip()
-    print ("Configuring Switch " + (IP))
-    HOST = IP
-    tn = telnetlib.Telnet(HOST)
-    tn.read_until(b"Username: ")
-    tn.write(user.encode('ascii') + b"\n")
-    if password:
+   IP=IP.strip()
+   print ("Configuring Switch " + (IP))
+   HOST = IP
+   tn = telnetlib.Telnet(HOST)
+   tn.read_until(b"Username: ")
+   tn.write(user.encode('ascii') + b"\n")
+   
+   # Login to switch
+   if password:
        tn.read_until(b"Password: ")
        tn.write(password.encode('ascii') + b"\n")
-    tn.write(b"conf t\n")
-    tn.write(b"vlan 2\n")
-    tn.write(b"vlan 2\n")
-    tn.write(b"name Python_VLAN_2\n")
-    tn.write(b"exit\n")
-    tn.write(b"vlan 3\n")
-    tn.write(b"name Python_VLAN_3\n")
-    tn.write(b"exit\n")
-    tn.write(b"vlan 4\n")
-    tn.write(b"name Python_VLAN_4\n")
-    tn.write(b"exit\n")
-    tn.write(b"vlan 5\n")
-    tn.write(b"name Python_VLAN_5\n")
-    tn.write(b"exit\n")
-    tn.write(b"vlan 6\n")
-    tn.write(b"name Python_VLAN_6\n")
-    tn.write(b"vlan 7\n")
-    tn.write(b"name Python_VLAN_7\n")
-    tn.write(b"vlan 8\n")
-    tn.write(b"name Python_VLAN_8\n")
-    tn.write(b"end\n")
-    tn.write(b"exit\n")
-    print(tn.read_all().decode('ascii'))
+   tn.write(b"conf t\n")
+
+   # Create and name VLANs 2-20
+   for i in range(2,21):
+      tn.write(b"vlan " + str(n).encode('ascii') + b"\n")
+      tn.write(b"name  Python_VLAN_"+str(n).encode('ascii')+b"\n")
+
+   # Exit global config mode
+   tn.write(b"end\n")
+   tn.write(b"exit\n")
+   print(tn.read_all().decode('ascii'))
